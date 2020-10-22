4bf34021ad6a0a35ef336d11201d379a7672270c
IviweMalotana <ivrite23@gmail.com>
2020-10-22 08:40:36 +0100

    Added new app.py and api.py

--- api.py
+++ api.py
@@ -1,53 +1,34 @@
+import mfrc522
 from mfrc522 import SimpleMFRC522
 import RPi.GPIO as GPIO
-import BlynkLib
 
 GPIO.setwarnings(False)
 reader = SimpleMFRC522()
+mfr = mfrc522.MFRC522()
 
-def start(code):
-	blynk = BlynkLib.Blynk(code)
+def AntennaOn():
+	mfr.AntennaOn()
 
-	# Add Member
-	@blynk.VIRTUAL_WRITE(1)
-	def AddMember(value):
-        	if value==[u'1']:
-			try:
-				blynk.virtual_write(10,"Type name of person to add:")
+def AntennaOff():
+	mfr.AntennaOff()
 
-				@blynk.VIRTUAL_WRITE(9)
-				def ReadInput(str):
-					text = ' '.join(str)
-					blynk.virtual_write(10,"Tap tag")
-					reader.write(text)
-					blynk.virtual_write(10,"Added the member "+text)
+def addMember(text):
+        try:
+		id,name = reader.write(text)
+		return name
+	finally:
+		GPIO.cleanup()
 
-					blynk.virtual_write(1,0)
-			finally:
-				GPIO.cleanup()
-	# GetID
-	@blynk.VIRTUAL_WRITE(2)
-	def getID(value):
-		if value==[u'1']:
-			try:
-				blynk.virtual_write(10,"Tap tag to get ID number")
-				id, text = reader.read()
-				blynk.virtual_write(10,id)
-				blynk.virtual_write(2,0)
-			finally:
-				GPIO.cleanup()
+def getID():
+	try:
+		id, text = reader.read()
+		return id
+	finally:
+		GPIO.cleanup()
 
-	# GetName
-	@blynk.VIRTUAL_WRITE(3)
-	def getPerson(value):
-		if value==[u'1']:
-			try:
-				blynk.virtual_write(10,"Tap tag to get name")
-				id, text = reader.read()
-				blynk.virtual_write(10,text)
-				blynk.virtual_write(3,0)
-			finally:
-				GPIO.cleanup()
-
-	while True:
-		blynk.run()
+def getName():
+	try:
+		id, text = reader.read()
+		return text
+	finally:
+		GPIO.cleanup()
--- app.py
+++ app.py
@@ -1,4 +1,43 @@
 import api
+import BlynkLib
 
 # Initialize Blynk
-api.start('9rvmtZDBOuQr0KrJ-FFwB2dZblwt3yDp')
+blynk = BlynkLib.Blynk('9rvmtZDBOuQr0KrJ-FFwB2dZblwt3yDp')
+
+# Turn on and off
+@blynk.VIRTUAL_WRITE(0)
+def Antenna(value):
+	if value==[u'0']:
+		api.AntennaOff()
+	elif value==[u'1']:
+		api.AntennaOn()
+# Add Member
+@blynk.VIRTUAL_WRITE(1)
+def AddMember(value):
+	if value==[u'1']:
+		blynk.virtual_write(10,"Type name of person to add:")
+		@blynk.VIRTUAL_WRITE(9)
+		def ReadInput(str):
+			text = ' '.join(str)
+			blynk.virtual_write(10,"Tap tag")
+			name = api.addMember(text)
+			blynk.virtual_write(10,"Added the member "+name)
+			blynk.virtual_write(1,0)
+# GetID
+@blynk.VIRTUAL_WRITE(2)
+def GetID(value):
+	if value==[u'1']:
+		blynk.virtual_write(10,"Tap tag to get ID number")
+		id = api.getID()
+		blynk.virtual_write(10,id)
+		blynk.virtual_write(2,0)
+# GetName
+@blynk.VIRTUAL_WRITE(3)
+def getPerson(value):
+	if value==[u'1']:
+		blynk.virtual_write(10,"Tap tag to get name")
+		text = api.getName()
+		blynk.virtual_write(10,text)
+		blynk.virtual_write(3,0)
+while True:
+                blynk.run()
